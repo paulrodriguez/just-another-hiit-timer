25daadc4524f77652e8a1169864df2281a06955b
Paul Rodriguez <paul.d.rodriguez@outlook.com>
2021-02-02 22:59:38 -0800

    feat: adding actions for redux

--- src/actions/exercise.ts
+++ src/actions/exercise.ts
@@ -1,3 +1,13 @@
+import WorkoutRepositoryFactory from '../repositories/WorkoutRepositoryFactory';
+let repository = WorkoutRepositoryFactory.create('local');
 export const addExercise = () {
-  
+  repository.addExercise()
+}
+
+export const deleteExercise = () {
+
+}
+
+export const sortExercises = () {
+
 }
